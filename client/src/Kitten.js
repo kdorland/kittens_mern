9d14b2f01c9b78fbe75daa25112a651760472627
Kristian <krdo@eaaa.dk>
2020-03-02 13:45:46 +0100

    Breaking one of the tests.

--- client/src/Kitten.js
+++ client/src/Kitten.js
@@ -9,16 +9,16 @@ class Kitten extends Component {
         let content = <p>Loading</p>;
         if (kitten) {
             content =
-                <React.Fragment>
+                <>
                     <h1>{kitten.name}</h1>
 
-                    <h3>Hobbies</h3>
+                    <h3>Pastimes</h3>
                     <ul>
                         {kitten.hobbies.map(h => <li key={h}>{h}</li>)}
                     </ul>
 
                     <Link to="/">Back</Link>
-                </React.Fragment>
+                </>
         }
 
         return content;
